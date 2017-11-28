81ea845a1a53445373ac4496636d14d379d03bfa
bibekanandagithub <31163713+bibekanandagithub@users.noreply.github.com>
2017-11-28 08:31:35 +0530

    nuget tested all

--- Tfswork/Program.cs
+++ Tfswork/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.TestManagement.Client;
-
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
 
 namespace Tfswork
 {
@@ -13,6 +13,10 @@ namespace Tfswork
     {
         static void Main(string[] args)
         {
+            Uri CollectionUri = (args.Length < 1) ? new Uri("http://desktop-anh3ro7:8080/tfs/DefaultCollection/") : new Uri(args[0]);
+            TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(CollectionUri);
+            WorkItemStore wis = tpc.GetService<WorkItemStore>();  // To get work items service
+            ITestManagementTeamProject tm = tpc.GetService<ITestManagementService>().GetTeamProject("TechBrothers");
         }
     }
 }
