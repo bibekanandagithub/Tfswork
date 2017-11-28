d6fa7311d24a6f48d4ec96314b1454ee709f39f9
bibekanandagithub <31163713+bibekanandagithub@users.noreply.github.com>
2017-11-28 07:59:18 +0530

    Add project files.

---
+++ Tfswork/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.TestManagement.Client;
+
+
+namespace Tfswork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
